04f4e5589cc83acc7bc9e100672efaccc59dafb7
18039253 <18039253@myrp.edu.sg>
2020-05-08 11:36:59 +0800

    Initial commit

---
+++ app/src/main/java/c346/rp/edu/sg/demoimplicitintent/MainActivity.java
@@ -0,0 +1,47 @@
+package c346.rp.edu.sg.demoimplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnEmail, btnRP;
+    EditText etMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEmail = (Button) findViewById(R.id.buttonEmail);
+        btnRP = (Button) findViewById(R.id.buttonRP);
+        etMessage = (EditText)findViewById(R.id.editTextMessage);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Test Email from C347");
+                email.putExtra(Intent.EXTRA_TEXT, etMessage.getText());
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client: "));
+            }
+        });
+
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+    }
+}
